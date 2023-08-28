72079e3025ca59df06609b085516894ef9e3bfcf
Dariaban <dariabandukova@gmail.com>
2023-08-29 01:46:30 +0300

    Второй вариант

--- src/main/java/Dish.java
+++ src/main/java/Dish.java
@@ -2,84 +2,178 @@ import java.util.Scanner;
 
 
 class Dish {
-    Scanner scanner = new Scanner(System.in);
-    Scanner scanner1 = new Scanner(System.in);
-    public String finish = "Завершить";
-    public double sumOfDishes;
-    public String name, check = "проверка";
+    public String name;
     public double price;
-    public String listOfNAmes = "";
-    public int numberOfPeople;
-    public double sumPerPerson;
 
+}
+    class Calculator {
+        Scanner scanner = new Scanner(System.in);
+        StringBuilder builder = new StringBuilder();
+        Formater formater = new Formater();
+        private static final String finish = "Завершить";
+        private double sumOfDishes;
+        private String check = "проверка";
+        private String listOfNAmes = "";
+        private int numberOfPeople;
+        private double sumPerPerson;
 
+        public int peoplesCalculator() {
+            do {
+                System.out.println("На скольких делим счет?");
+                checkPeople();
+                numberOfPeople = scanner.nextInt();
+                if (numberOfPeople <= 1) {
+                    System.out.println("Слишком мало людей! Попробуем снова.");
+                } else {
+                    System.out.println("Делим на " + numberOfPeople + " человек.");
+                }
+            } while (numberOfPeople < 1);
 
-    public int peoplesCalculator() {
-        do {System.out.println("На скольких делим счет?");
-            peoplescalculator2();
-            numberOfPeople = scanner.nextInt();
-            if (numberOfPeople <= 1) {
-                System.out.println("Слишком мало людей! Попробуем снова.");
-            } else {
-                System.out.println("Делим на " + numberOfPeople + " людей.");
+            return numberOfPeople;
+        }
+
+        public void checkPeople() {
+            while (!scanner.hasNextInt()) {
+                System.out.println("Что-то пошло не так! Введите число людей");
+                System.out.println("На скольких делим счет?");
+                scanner.next();
             }
-        }while (numberOfPeople <= 2);
 
-        return numberOfPeople;
-    }
-    public void peoplescalculator2(){
-        while (!scanner.hasNextInt()){
-            System.out.println("Что-то пошло не так! Введите число людей");
-            System.out.println("На скольких делим счет?");
-            scanner.next();
         }
 
-    }
-
-    public String askName() {
-        System.out.println("Введите название блюда");
-        while (!scanner.hasNext()) {
-            System.out.println("Что-то пошло не так!");
+        public String askName(String name) {
             System.out.println("Введите название блюда");
-            scanner.next();
-        }
-        name = scanner.next();
-        listOfNAmes = listOfNAmes + "\n" + name;
+            while (!scanner.hasNext()) {
+                System.out.println("Что-то пошло не так!");
+                System.out.println("Введите название блюда");
+                scanner.next();
+            }
+            name = scanner.next();
+            listOfNAmes = listOfNAmes + "\n" + name;
 
-        return listOfNAmes;
+            return listOfNAmes;
+        }
+
+        public double calculator(double price) {
+           do {
+               System.out.println("Введите цену в формате рубли,копейки");
+               checkPrice();
+               price = scanner.nextDouble();
+               if (price <= 0) {
+                   System.out.println("Цена не может нулевой или отрицательной");
+                   System.out.println("Введите цену в формате рубли,копейки");
+                   scanner.next();
+               } else {
+                   sumOfDishes = sumOfDishes + price;
+                   System.out.println("Вы заказали: " + listOfNAmes + " " + price + " на сумму; " + sumOfDishes);
+               }
+           }while (price <=0);
+            return sumOfDishes;
+        }
+public void checkPrice(){
+    while (!scanner.hasNextDouble()) {
+        System.out.println("Что-то пошло не так!");
+        System.out.println("Введите цену в формате рубли,копейки");
+        scanner.next();
+    }
+}
+
+        public void calculator2() {
+            do {
+                askName(null);
+                calculator(0.0);
+                System.out.println("Добавить следующее или завершить?");
+                check = scanner.next();
+            }
+            while (!check.equalsIgnoreCase(finish));
+        }
+
+        public void result() {
+            double number = (int) numberOfPeople;
+
+            sumPerPerson = sumOfDishes / number;
+            StringBuilder builder = new StringBuilder("Вы заказали " + "\n" + listOfNAmes + "\n" + "Общая стоимость стоимость заказа " + String.format("%.2f", Math.floor(sumOfDishes)) + " " + formater.formaterSum(sumOfDishes) + "." + " " + "Каждый заплатит " + String.format("%.2f", Math.floor(sumPerPerson)) + " " + formater.formaterSumPerPerson(sumPerPerson) +".");
+            System.out.println(builder);
+        }
+
+    }
+class Formater {
+    public String format;
+    public String formaterSum(double sumOfDishes) {
+        int rub = (int) (sumOfDishes % 10);
+
+        switch (rub) {
+            case (1):
+                format = "рубль";
+                break;
+            case (2):
+                format = "рубля";
+                break;
+            case (3):
+                format = "рубля";
+                break;
+            case (4):
+                format ="рубля";
+                break;
+            case (5):
+                format ="рублей";
+                break;
+            case (6):
+                format ="рублей";
+                break;
+            case (7):
+                format = "рублей";
+                break;
+            case (8):
+                format ="рублей";
+                break;
+            case (9):
+                format ="рублей";
+                break;
+        }
+        return format;
     }
 
-    public double calculator() {
-        System.out.println("Введите стоимость в формате рубли,копейки");
-        while (!scanner.hasNextDouble()){
-            System.out.println("Что-то пошло не так!");
-            System.out.println("Введите стоимость в формате рубли,копейки");
-            scanner.next();
+    public String formaterSumPerPerson(double sumPerPerson) {
+        int rub = (int) (sumPerPerson % 10);
+
+        switch (rub) {
+            case (1):
+                format = "рубль";
+            break;
+            case (2):
+                format = "рубля";
+            break;
+            case (3):
+                format = "рубля";
+            break;
+            case (4):
+                format ="рубля";
+            break;
+            case (5):
+                format ="рублей";
+            break;
+            case (6):
+                format ="рублей";
+            break;
+            case (7):
+                format = "рублей";
+            break;
+            case (8):
+                format ="рублей";
+            break;
+            case (9):
+                format ="рублей";
+            break;
         }
-        price = scanner.nextDouble();
-        sumOfDishes = sumOfDishes + price;
-
-        System.out.println("Вы заказали: " + listOfNAmes + " " + price + " на сумму; " + sumOfDishes);
-        return sumOfDishes;
-    }
-
-
-    public void calculator2() {
-        do {
-            askName();
-            calculator();
-            System.out.println("Добавить следующее или завершить?");
-            check = scanner1.nextLine();
-        }
-        while (!check.equalsIgnoreCase(finish));
-    }
-
-    public void result() {
-        sumPerPerson = sumOfDishes / numberOfPeople;
-        System.out.println("Вы заказали " + "\n" + listOfNAmes + "\n" + ". Общая стоимость стоимость заказа " + sumOfDishes + " " + "Каждый заплатит " + String.format("%.2f", Math.floor(sumPerPerson)) +" "+ "рубля");
+        return format;
     }
 
 }
+
+
+
+
 
 
 
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,12 @@
-import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int numberOfPeople = 0;
         Dish dish = new Dish();
-        dish.peoplesCalculator();
-        dish.calculator2();
-        dish.result();
+        Calculator calculator = new Calculator();
+        calculator.peoplesCalculator();
+        calculator.calculator2();
+        calculator.result();
+
     }
 }
 
